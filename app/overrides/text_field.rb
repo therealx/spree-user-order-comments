1b362f855adad8f111809753a18f741d9c82aa7b
Christian Woodward <christianwoodward@mac.com>
2012-07-27 17:49:37 -0700

    Fixed Path

--- app/overrides/text_field.rb
+++ app/overrides/text_field.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/payment/edit",
+Deface::Override.new(:virtual_path => "spree/payment/_delivery",
                      :name => "add_text_field_to_shipping_20120112",
                      :insert_before => "[data-hook='buttons']",
                      :partial => "spree/orders/comment",
