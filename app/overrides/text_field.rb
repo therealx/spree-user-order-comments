c578246b08a6123218335692b72c4f074bbc59f0
Christian Woodward <christianwoodward@mac.com>
2012-07-27 17:32:18 -0700

    Edit Path

--- app/overrides/text_field.rb
+++ app/overrides/text_field.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/orders/edit",
+Deface::Override.new(:virtual_path => "spree/payment/edit",
                      :name => "add_text_field_to_shipping_20120112",
                      :insert_before => "[data-hook='buttons']",
                      :partial => "spree/orders/comment",
