3a7c89a612ed8c67ad03b8cc5cb3f412dc9ea95e
Christian Woodward <christianwoodward@mac.com>
2012-07-27 18:03:14 -0700

    Revert to orig
    Git is being a pain and won't let me revert

--- app/overrides/text_field.rb
+++ app/overrides/text_field.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/payment/_delivery",
-                     :name => "add_text_field_to_shipping_20120112",
-                     :insert_before => "[data-hook='buttons']",
+Deface::Override.new(:virtual_path => "spree/orders/edit",
+                     :name => "add_text_field_to_cart_20120112",
+                     :insert_before => "[data-hook='cart_buttons']",
                      :partial => "spree/orders/comment",
                      :disabled => false)
